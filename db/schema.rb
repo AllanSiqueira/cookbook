6013abc5f6a125f820d180157b86e879e3cd1661
Allan Siqueira <allansiqueiraster@hotmail.com>
2018-07-04 17:58:00 -0300

    Usuário cadastra suas receitas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_164838) do
+ActiveRecord::Schema.define(version: 2018_07_04_205046) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_04_164838) do
     t.datetime "updated_at", null: false
     t.text "ingredients"
     t.text "method"
+    t.text "cook_method"
   end
 
 end
--- spec/features/visitor_view_recipe_details_spec.rb
+++ spec/features/visitor_view_recipe_details_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 feature 'Visitor view recipe details' do
-
   scenario 'successfully' do
-
+    #cria os dados necessários
     recipe = Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
                           cuisine: 'Brasileira', difficulty: 'Médio',
                           cook_time: 60,
                           ingredients: 'Farinha, açucar, cenoura',
-                          method: 'Cozinhe a cenoura, corde em pedeços (...)')
+                          cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
+    # simula a ação do usuário
     visit root_path
     click_on recipe.title
 
+    # expectativas do usuário após a ação
     expect(page).to have_css('h1', text: recipe.title)
     expect(page).to have_css('h3', text: 'Detalhes')
     expect(page).to have_css('p', text: recipe.recipe_type)
@@ -22,20 +23,23 @@ feature 'Visitor view recipe details' do
     expect(page).to have_css('h3', text: 'Ingredientes')
     expect(page).to have_css('p', text: recipe.ingredients)
     expect(page).to have_css('h3', text: 'Como Preparar')
-    expect(page).to have_css('p', text: recipe.method)
+    expect(page).to have_css('p', text: recipe.cook_method)
   end
-  scenario 'and return to recipe list' do
 
+  scenario 'and return to recipe list' do
+    #cria os dados necessários
     recipe = Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
                           cuisine: 'Brasileira', difficulty: 'Médio',
                           cook_time: 60,
                           ingredients: 'Farinha, açucar, cenoura',
-                          method: 'Cozinhe a cenoura, corde em pedeços (...)')
+                          cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
+    # simula a ação do usuário
     visit root_path
     click_on recipe.title
     click_on 'Voltar'
 
+    # expectativa da rota atual
     expect(current_path).to eq(root_path)
   end
 end
