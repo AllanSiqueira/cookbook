46b97b95bfa92bb2e8c6752822a8dd5b150e01f8
Allan Siqueira <allansiqueiraster@hotmail.com>
2018-07-06 14:05:13 -0300

    Administrador cadastra cozinhas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,18 +3,19 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find params[:id]
   end
   def new
+    @cuisines = Cuisine.all
     @recipe = Recipe.new
   end
   def create
-    @recipe = Recipe.new
-    @recipe.title = params[:recipe][:title]
-    @recipe.recipe_type = params[:recipe][:recipe_type]
-    @recipe.ingredients = params[:recipe][:ingredients]
-    @recipe.method = params[:recipe][:method]
-    @recipe.cuisine = params[:recipe][:cuisine]
-    @recipe.difficulty = params[:recipe][:difficulty]
-    @recipe.cook_time = params[:recipe][:cook_time]
+    @recipe = Recipe.new recipes_params
     @recipe.save
     redirect_to recipe_url(@recipe)
+  end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine_id,:difficulty,
+                                  :cook_time, :ingredients, :cook_method)
   end
 end
